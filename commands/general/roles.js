1bd7695940ae7279225595e6b987fc472f4731f0
Mgram <marcus@ingrams.cloud>
2023-02-12 17:30:46 -0800

    restructured

--- commands/general/roles.js
+++ commands/general/roles.js
@@ -4,8 +4,7 @@ const Discord = require("discord.js");
 module.exports = {
     data: new Discord.SlashCommandBuilder()
     .setName(`roles`)
-    .setDescription(`Select your preferred main role`)
-    .setDefaultPermission(false),
+    .setDescription(`Select your preferred main role`),
     permissions: 2,
     async execute (interaction) {
         const returnEmbed = new Discord.EmbedBuilder()
@@ -18,15 +17,19 @@ module.exports = {
         .addComponents(new Discord.ButtonBuilder().setCustomId('healrole').setLabel('Healer').setStyle(Discord.ButtonStyle.Success).setEmoji("<:RoleHealer:1073482462505746452>"),)
         .addComponents(new Discord.ButtonBuilder().setCustomId('dpsrole').setLabel('DPS').setStyle(Discord.ButtonStyle.Success).setEmoji("<:RoleDPS:1073482460605714492>"),)
         .addComponents(new Discord.ButtonBuilder().setCustomId('norole').setLabel('No Job Role').setStyle(Discord.ButtonStyle.Success),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorpink').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorpurple').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorblue').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorgreen').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('coloryellow').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorred').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colororange').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
-	.addComponents(new Discord.ButtonBuilder().setCustomId('colorcyan').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
 
-        interaction.reply({ embeds: [returnEmbed], components: [row] });
+        const row2 = new Discord.ActionRowBuilder()
+        .addComponents(new Discord.ButtonBuilder().setCustomId('colorpink').setLabel('Pink').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorpurple').setLabel('Purple').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorblue').setLabel('Blue').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorgreen').setLabel('Green').setStyle(Discord.ButtonStyle.Primary),)
+
+        const row3 = new Discord.ActionRowBuilder()
+	.addComponents(new Discord.ButtonBuilder().setCustomId('coloryellow').setLabel('Yellow').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorred').setLabel('Red').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colororange').setLabel('Orange').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorcyan').setLabel('Cyan').setStyle(Discord.ButtonStyle.Primary),)
+
+        interaction.reply({ embeds: [returnEmbed], components: [row, row2, row3] });
     }
 }
--- index.js
+++ index.js
@@ -93,7 +93,7 @@ bot.on('interactionCreate', async interaction => {
 		}
 	}
 	
-	function removeColors() {
+	async function setColors(newColor) {
 		interaction.member.roles.remove("1074484970158030879")
 		interaction.member.roles.remove("1074485067302309959")
 		interaction.member.roles.remove("1074485131357720626")
@@ -102,71 +102,61 @@ bot.on('interactionCreate', async interaction => {
 		interaction.member.roles.remove("1074485296743321710")
 		interaction.member.roles.remove("1074485344151552082")
 		interaction.member.roles.remove("1074485432089313411")
+		interaction.member.roles.add(`${newColor}`)
 	}
 
 	if (interaction.isButton()) {
+		interaction.deferUpdate();
 		switch (interaction.customId) {
 			case "tankrole":
-				interaction.deferUpdate();
 				interaction.member.roles.add("971728838100910140")
 				interaction.member.roles.remove("971728859017920513")
 				interaction.member.roles.remove("971728899547480125")
 				break;
 			case "healrole":
-				interaction.deferUpdate();
 				interaction.member.roles.add("971728859017920513")
 				interaction.member.roles.remove("971728838100910140")
 				interaction.member.roles.remove("971728899547480125")
 				break;
 			case "dpsrole":
-				interaction.deferUpdate();
 				interaction.member.roles.add("971728899547480125")
 				interaction.member.roles.remove("971728838100910140")
 				interaction.member.roles.remove("971728859017920513")
 				break;
 			case "norole":
-				interaction.deferUpdate();
 				interaction.member.roles.remove("971728899547480125")
 				interaction.member.roles.remove("971728838100910140")
 				interaction.member.roles.remove("971728859017920513")
 				break;
 			case "colorpink":
-				removeColors();
-				interaction.member.roles.add("1074484970158030879")
+				setColors("1074484970158030879")
 				break;
 			case "colorpurple":
-				removeColors();
-				interaction.member.roles.add("1074485067302309959")
+				setColors("1074485067302309959")
 				break;
 			case "colorblue":
-				removeColors();
-				interaction.member.roles.add("1074485131357720626")
+				setColors("1074485131357720626")
 				break;
 			case "colorgreen":
-				removeColors();
-				interaction.member.roles.add("1074485178174550017")
+				setColors("1074485178174550017")
 				break;
 			case "coloryellow":
-				removeColors();
-				interaction.member.roles.add("1074485239398813756")
+				setColors("1074485239398813756")
 				break;
 			case "colorred":
-				removeColors();
-				interaction.member.roles.add("1074485296743321710")
+				setColors("1074485296743321710")
 				break;
 			case "colororange":
-				removeColors();
-				interaction.member.roles.add("1074485344151552082")
+				setColors("1074485344151552082")
 				break;
 			case "colorcyan":
-				removeColors();
-				interaction.member.roles.add("1074485432089313411")
+				setColors("1074485432089313411")
 				break;
 			default:
 				console.log('no matching option found for button pressed')
 			
 		}
-		console.log(`Role assignment used by ${interaction.member.tag}`)
+		console.log(`Role assignment used by ${interaction.member}`)
 	}
 });
 
