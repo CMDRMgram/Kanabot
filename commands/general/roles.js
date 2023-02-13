ee8a1ae2fe357651fc3269ef0cc25e27b9c93374
Mgram <85346345+CMDRMgram@users.noreply.github.com>
2023-02-13 11:29:01 +1100

    Update roles.js

--- commands/general/roles.js
+++ commands/general/roles.js
@@ -11,13 +11,21 @@ module.exports = {
         const returnEmbed = new Discord.EmbedBuilder()
 		.setColor('#FF7100')
 		.setTitle("**Role Selection**")
-		.setDescription(`Use the buttons below to select your primary Job Role.`)
+		.setDescription(`Use the buttons below to select your primary Job Role and Color.`)
 
         const row = new Discord.ActionRowBuilder()
         .addComponents(new Discord.ButtonBuilder().setCustomId('tankrole').setLabel('Tank').setStyle(Discord.ButtonStyle.Success).setEmoji("<:RoleTank:1073482465462726668>"),)
         .addComponents(new Discord.ButtonBuilder().setCustomId('healrole').setLabel('Healer').setStyle(Discord.ButtonStyle.Success).setEmoji("<:RoleHealer:1073482462505746452>"),)
         .addComponents(new Discord.ButtonBuilder().setCustomId('dpsrole').setLabel('DPS').setStyle(Discord.ButtonStyle.Success).setEmoji("<:RoleDPS:1073482460605714492>"),)
-        .addComponents(new Discord.ButtonBuilder().setCustomId('norole').setLabel('None').setStyle(Discord.ButtonStyle.Success),)
+        .addComponents(new Discord.ButtonBuilder().setCustomId('norole').setLabel('No Job Role').setStyle(Discord.ButtonStyle.Success),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorpink').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorpurple').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorblue').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorgreen').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('coloryellow').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorred').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colororange').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
+	.addComponents(new Discord.ButtonBuilder().setCustomId('colorcyan').setLabel('None').setStyle(Discord.ButtonStyle.Primary),)
 
         interaction.reply({ embeds: [returnEmbed], components: [row] });
     }
